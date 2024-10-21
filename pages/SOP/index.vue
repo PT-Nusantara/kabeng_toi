3dcd4d6e7dd5227d9f85783429017a3579125b61
azrillll <azrilmaulana1028@gmail.com>
2024-10-21 16:08:16 +0700

    first commit

---
+++ pages/SOP/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+      <div class="header">
+        <div class="header-left">
+          <img src="smkn4.jpg" alt="smkn4" />
+          <div class="header-text">
+            <h3>Kabeng Toi</h3>
+            <p>JI.Depok, Sukamenak, Purbaratu</p>
+            <p>catatan kepala bengkel toi SMKN 4 Tasikmalaya</p>
+          </div>
+        </div>
+        <div class="header-right">
+          <img src="toi.jpg" alt="Logo Teknik Elektronika" />
+        </div>
+      </div>
+      <div class="body">
+        <div class="back-button">
+          <i class="fas fa-arrow-left"></i>
+        </div>
+        <div class="content">
+          <h2>Riwayat</h2>
+          <div class="riwayat">
+            <!-- Content for riwayat goes here -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  
+  
---
+++ pages/webpages/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+      <div class="header">
+        <div class="logo">
+          <img src="toi.jpg" alt="Logo">
+        </div>
+        <div class="title">
+          <h1>Kabeng Toi</h1>
+          <p>Jl. Depok, Sukamenak, Purbaratu</p>
+          <p>catatan kepala bengkel toi SMKN 4 Tasikmalaya</p>
+        </div>
+        <div class="right">
+          <img src="logo-right.png" alt="Logo Right">
+        </div>
+      </div>
+      <div class="content">
+        <div class="back">
+          <i class="fa fa-arrow-left"></i>
+        </div>
+        <div class="menu">
+          <div class="menu-item" @click="gotoPinjamAlat">
+            <p>PINJAM ALAT</p>
+          </div>
+          <div class="menu-item" @click="gotoPinjamRuang">
+            <p>PINJAM RUANG</p>
+          </div>
+          <div class="menu-item" @click="gotoSOP">
+            <p>SOP</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      gotoPinjamAlat() {
+        // Implement logic to navigate to Pinjam Alat page
+        console.log('Navigating to Pinjam Alat');
+      },
+      gotoPinjamRuang() {
+        // Implement logic to navigate to Pinjam Ruang page
+        console.log('Navigating to Pinjam Ruang');
+      },
+      gotoSOP() {
+        // Implement logic to navigate to SOP page
+        console.log('Navigating to SOP');
+      }
+    }
+  };
+  </script>
+  
+ 
+ 
